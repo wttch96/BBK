7f89324d9fd3ff863c0b85dd06f8b3148411ef7f
wttch <wttch@wttch.com>
2024-06-14 08:27:00 +0800

    Initial Commit

---
+++ BBK/BBKApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBKApp.swift
+//  BBK
+//
+//  Created by Wttch on 2024/6/14.
+//
+
+import SwiftUI
+
+@main
+struct BBKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
